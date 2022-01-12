5675b25342a4c419ce69cc5cc4d6ab242b05831b
Daniel <daniel.hermansson@gmail.com>
2022-01-12 23:36:16 +0100

    init

---
+++ CodeWarsKata/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("CodeWarsKata project. ");
+Console.WriteLine("Write Kata in Kata.cs");
+Console.WriteLine("Add tests to Test.cs");
+Console.WriteLine("Test with 'dotnet test'");
---
+++ CodeWarsKata/Tests.cs
@@ -0,0 +1,11 @@
+using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class KataTests {
+
+[Test]
+  public void Test1() {
+    Assert.IsTrue(true);
+  }
+}
